48374530369c34beac4dab1752c80976cbc6fa30
ItsKensann <itskensann@gmail.com>
2024-06-11 16:32:32 -0700

    assignment endpoints
    implemented basic assignment endpoints

--- api/index.js
+++ api/index.js
@@ -3,11 +3,13 @@ const { Router } = require("express");
 // Importing sub routers for resources
 const usersRouter = require("./users")
 const coursesRouter = require("./courses")
+const assignmentsRouter = require("./assignments")
 
 const router = Router();
 
 // Mounting the sub routers for specific paths
 router.use("/users", usersRouter)
 router.use("/courses", coursesRouter)
+router.use("/assignments", assignmentsRouter)
 
 module.exports = router;
